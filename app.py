e7dcd3b1b1a81a158c3b3306dafd3e340bc28549
jeongwool <163831081+jeongwool@users.noreply.github.com>
2025-11-28 03:02:00 +0900

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ if not GOOGLE_API_KEY or not STABILITY_API_KEY:
 genai.configure(api_key=GOOGLE_API_KEY)
 
 # 최신 모델
-CHAT_MODEL_NAME = "gemini-1.5-flash"
+CHAT_MODEL_NAME = "models/gemini-1.5-flash"
 
 STABILITY_API_HOST = "https://api.stability.ai"
 STABILITY_ENGINE_ID = "stable-diffusion-xl-1024-v1-0"
